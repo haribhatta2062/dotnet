6b68bfd3362fa06cf33807d213685d51d32eb89c
haribhatta2062 <bhattaharry0000@gmail.com>
2025-07-06 14:11:17 +0545

    this is first csharp git commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// using system;
+// namespace Program
+
+    class GitDemo
+    {
+        public static void Main()
+        {
+            Console.WriteLine("this is the first git commit in csharp");
+        }
+    }
+
