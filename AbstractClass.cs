4df2f060280c7c963df7b6b3494ab895de1a2d6c
haribhatta2062 <bhattaharry0000@gmail.com>
2025-07-07 13:21:28 +0545

    abstract class example

---
+++ AbstractClass.cs
@@ -0,0 +1,25 @@
+// using System;
+namespace LearnAbstractClass
+{
+    public abstract class LearnAbstract
+    {
+        static void Main()
+        {
+            AbstractChild child1 = new AbstractChild();
+            child1.Read();
+            child1.WithBody();
+        }
+        public abstract void Read();
+        public  void WithBody()
+        {
+            Console.WriteLine("this is the another method in abstract class with body");
+        }
+    }
+    public class AbstractChild : LearnAbstract
+    {
+        public override void Read()
+        {
+            Console.WriteLine("this the is the abstract class at child class");
+        }
+     }
+    }
