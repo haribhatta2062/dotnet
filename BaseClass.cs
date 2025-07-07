e69eefc1255097940d65b871a63a02d37b5614c3
haribhatta2062 <bhattaharry0000@gmail.com>
2025-07-07 19:35:46 +0545

    this is the example of Base class

---
+++ BaseClass.cs
@@ -0,0 +1,26 @@
+﻿namespace LearnBaseclass
+{
+    public class LearnBase
+    {
+        public LearnBase()
+        {
+            Console.WriteLine("this is the constructor with no parameter");
+        }
+        public LearnBase(string name)
+        {
+            Console.WriteLine("my name is " + name);
+        }
+    }
+    public class chilldClass : LearnBase {
+        public chilldClass() :base() {
+            Console.WriteLine("constructor called");
+        }
+    
+    
+        static void Main()
+        {
+            chilldClass chilldClass = new chilldClass();
+
+         }
+    }  
+    }
